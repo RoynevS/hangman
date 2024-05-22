a44d617d8faf77e45a197f49834b14bc26eedda5
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-05-22 12:35:48 +0200

    Create game object

---
+++ src/game-controller.js
@@ -0,0 +1,50 @@
+function gameController(word) {
+  let tries = 0;
+
+  const getTries = () => tries;
+  const increaseTries = () => tries++;
+  const getWord = () => word;
+  const splitWord = word.split("");
+
+  let wordState = setStartingEncryption(word);
+
+  const getWordState = () => wordState;
+
+  const checkCharInWord = (char) => {
+    if (splitWord.includes(char)) {
+      setWordState(char);
+    } else {
+      increaseTries();
+    }
+  };
+
+  const setWordState = (char) => {
+    splitWord.forEach((element, index) => {
+      if (element === char) wordState[index] = char;
+    });
+  };
+
+  const checkGameEnd = () => {
+    if (!wordState.includes("_")) {
+      return "win";
+    } else if (getTries() >= 10) {
+      return "lose";
+    }
+    return false;
+  };
+
+  return {
+    getTries,
+    getWord,
+    getWordState,
+    setWordState,
+    checkCharInWord,
+    checkGameEnd,
+  };
+}
+
+function setStartingEncryption(word) {
+  return word.split("").map(() => "_");
+}
+
+export { gameController };
